e359407b7691180da695bc66e5aa32418248a12c
Muhammad Ramazan <lightify97@yahoo.com>
2025-05-26 00:00:43 +0500

    Update posthog provider variable

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -1,15 +1,15 @@
 "use client"
 
+import { usePathname, useSearchParams } from "next/navigation"
 import posthog from "posthog-js"
 import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react"
 import { Suspense, useEffect } from "react"
-import { usePathname, useSearchParams } from "next/navigation"
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: "/ingest",
-      ui_host: "https://us.posthog.com",
+      ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       capture_pageview: false, // We capture pageviews manually
       capture_pageleave: true, // Enable pageleave capture
       capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
